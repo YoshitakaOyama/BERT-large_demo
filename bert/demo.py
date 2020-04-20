2653ddfdb46a51346f7df079bea6926390d64c63
Yoshitaka Oyama <yo.0201.yo@gmail.com>
2020-04-20 14:09:54 +0900

    add explanation

--- bert/demo.py
+++ bert/demo.py
@@ -4,46 +4,26 @@ from transformers import BertTokenizer
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import textwrap
 from pprint import pprint
 import sys
+import textwrap
 
-# $$$ 参考にしたurl $$$
-URL = "https://mccormickml.com/2020/03/10/question-answering-with-a-fine-tuned-BERT/"
-
-# === 微調整された Bert-largeモデルのロード ===
-# 質問応答については、SQuADベンチマーク用にすでに微調整されたバージョンのBERT-largeがある
-# BERT-largeは本当に大きい。24層で埋め込みサイズは1,024で、合計で340Mのパラメーター, 全体で1.34GB
-# 注：このモデルはSQuADのバージョン1でトレーニングされた
+# Load BERT-large model
 model = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-# ======
 
-# === トークナイザーのロード ===
-# このモデルの語彙は、bert-base-uncasedの語彙とまったく同じ
+# Load BERT-large tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-# ======
 
-# === 解きたい質問とその回答を含んだ文章(リファレンス) ===
-# QAの例は、質問とその質問への回答を含むテキストの節で構成される
-# 我々は両方に対してBERTのトークナイザを実行する必要がある
+# Question and sentence containing the question
 question = "How many parameters does BERT-large have?"
 answer_text = "BERT-large is really big... it has 24-layers and an embedding size of 1,024, for a total of 340M parameters! Altogether it is 1.34GB, so expect it to take a couple minutes to download to your Colab instance."
-# ======
 
-# === 質問とリファレンスに対して、[SEP]トークンを配置し、ID付けする ===
-# これらをBERTにフィードするために、実際にそれらを連結し、その間に特別な[SEP]トークンを配置
-# トークナイザーのencodeで一瞬でidづけできる
-# Apply the tokenizer to the input text, treating them as a text-pair.
+# Place [SEP] token and assign ID
 input_ids = tokenizer.encode(question, answer_text)
 print('The input has a total of {:} tokens.'.format(len(input_ids)))
-# ======
 
-# === トークナイザーの把握 ===
-# トークナイザーが何をしているかを正確に確認するために、IDを付けてトークンを出力
-# BERT only needs the token IDs, but for the purpose of inspecting the 
-# tokenizer's behavior, let's also get the token strings and display them.
+# Grasp the tokenizer
 tokens = tokenizer.convert_ids_to_tokens(input_ids)
-
 # For each token and its id...
 for token, id in zip(tokens, input_ids):
     
@@ -57,74 +37,41 @@ for token, id in zip(tokens, input_ids):
     if id == tokenizer.sep_token_id:
         print('')
 
-# ちなみに出力見てみると ## が埋め込まれているけど、これは繋がっている単語の時に付与される
-# ======
-
-# === A, Bの埋め込み ===
-# いわゆる segment embedding と呼ばれるものですな
-# We’ve concatenated the question and answer_text together, 
-# but BERT still needs a way to distinguish them. 
-# BERT has two special “Segment” embeddings, one for segment “A” and one for segment “B”. 
-# Before the word embeddings go into the BERT layers, 
-# the segment A embedding needs to be added to the question tokens, and 
-# the segment B embedding needs to be added to each of the answer_text tokens.
-
-# These additions are handled for us by the transformer library, 
-# and all we need to do is specify a ‘0’ or ‘1’ for each token.
-# Note: In the transformers library, huggingface likes to call these token_type_ids, 
-# but I’m going with segment_ids since this seems clearer, and is consistent with the BERT paper.
-
-# [SEP]トークンIDのインデックス
 # Search the input_ids for the first instance of the `[SEP]` token.
 sep_index = input_ids.index(tokenizer.sep_token_id)
 
-# セグメントAの数([SEP]トークン自身はAに含まれる)
 # The number of segment A tokens includes the [SEP] token istelf.
 num_seg_a = sep_index + 1
 
-# セグメントBは残りだよね
 # The remainder are segment B.
 num_seg_b = len(input_ids) - num_seg_a
 
-# セグメントA, Bを0, 1で表現し、それのリストを作る
-# Construct the list of 0s and 1s.
+# We need to do is specify a ‘0’ or ‘1’ for each token.
 segment_ids = [0]*num_seg_a + [1]*num_seg_b
 
-# セグメントA + Bの数はトークンの数(今回は70個かな)と一致する
 # There should be a segment_id for every input token.
 assert len(segment_ids) == len(input_ids)
 
-# ======
-
-# === モデルを試す ===
-# Run our example through the model.
-# 上記で定義したモデルにinput_idsとセグメントリストを渡すと、スタートとエンドのスコア一覧のリストが返ってくる！ 超簡単
+# Run example through the model.
 start_scores, end_scores = model(torch.tensor([input_ids]), # The tokens representing our input text.
                                  token_type_ids=torch.tensor([segment_ids])) # The segment IDs to differentiate question from answer_text
 
-# scoresがどんな感じか確認
+# Check what scores look like
 print("start scores: ")
-print(f"length -> {len(start_scores)}") # tensor型なので 1
+print(f"length -> {len(start_scores)}")
 pprint(start_scores)
 print("--------------")
 print("end scores: ")
-print(f"length -> {len(end_scores)}") # tensor型なので 1
+print(f"length -> {len(end_scores)}")
 pprint(end_scores)
 
-# argmaxをとり、スタートとエンドのIDを取得
 # Find the tokens with the highest `start` and `end` scores.
 answer_start = torch.argmax(start_scores)
 answer_end = torch.argmax(end_scores)
 
-# Combine the tokens in the answer and print it out.
-answer = ' '.join(tokens[answer_start:answer_end+1])
-
-# print('Answer: "' + answer + '"') <-- ## 付きだけど、答え出せた！
-
 # Start with the first token.
 answer = tokens[answer_start]
 
-# subwordトークンである##をなくして綺麗に出力
 # Select the remaining answer tokens and join them with whitespace.
 for i in range(answer_start + 1, answer_end + 1):
     
@@ -138,9 +85,7 @@ for i in range(answer_start + 1, answer_end + 1):
 
 print('Answer: "' + answer + '"')
 
-# ======
-
-# === スコアのvisuallize化(barplot) ※スタートとエンド別々に描画 ===
+# Visualizing Scores
 # Use plot styling from seaborn.
 sns.set(style='darkgrid')
 
@@ -148,26 +93,24 @@ sns.set(style='darkgrid')
 #sns.set(font_scale=1.5)
 plt.rcParams["figure.figsize"] = (16,8)
 
-# 上記で確認したようにtensor型なので、描画するためフラットンする
 # Pull the scores out of PyTorch Tensors and convert them to 1D numpy arrays.
 s_scores = start_scores.detach().numpy().flatten()
 e_scores = end_scores.detach().numpy().flatten()
 print("s_scores: ")
 pprint(s_scores)
-print(f"length: {len(s_scores)}") # フラットンしたので当然70になる
+print(f"length: {len(s_scores)}")
 print("---------------------------")
 print("e_scores: ")
 pprint(e_scores)
-print(f"length: {len(e_scores)}") # フラットンしたので当然70になる
+print(f"length: {len(e_scores)}")
 
-# トークンラベルの付与
 # We'll use the tokens as the x-axis labels. In order to do that, they all need
 # to be unique, so we'll add the token index to the end of each one.
 token_labels = []
 for (i, token) in enumerate(tokens):
     token_labels.append('{:} - {:>2}'.format(token, i))
 
-# スタートスコア
+# Start score
 # Create a barplot showing the start word score for all of the tokens.
 ax = sns.barplot(x=token_labels, y=s_scores, ci=None)
 
@@ -179,10 +122,10 @@ ax.grid(True)
 
 plt.title('Start Word Scores')
 
-# plt.show()
 plt.savefig('./output/start_word_score.png')
+plt.show()
 
-# エンドスコア
+# End score
 # Create a barplot showing the end word score for all of the tokens.
 ax = sns.barplot(x=token_labels, y=e_scores, ci=None)
 
@@ -194,14 +137,9 @@ ax.grid(True)
 
 plt.title('End Word Scores')
 
-# plt.show()
 plt.savefig('./output/end_word_score.png')
-# ======
+plt.show()
 
-# === スコアのvisuallize化(barplot) ※スタートとエンド一緒に描画 ===
-# Store the tokens and scores in a DataFrame. 
-# Each token will have two rows, one for its start score and one for its end
-# score. The "marker" column will differentiate them. A little wacky, I know.
 scores = []
 for (i, token_label) in enumerate(token_labels):
 
@@ -230,4 +168,4 @@ g.set_xticklabels(g.ax.get_xticklabels(), rotation=90, ha="center")
 g.ax.grid(True)
 
 plt.savefig('./output/start_end_score.png')
-# ======
+plt.show()
